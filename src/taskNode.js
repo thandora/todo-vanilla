a04c99286256e30e96bf08748082925ad6be5d52
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-24 19:21:36 +0800

    create tasknode function

---
+++ src/taskNode.js
@@ -0,0 +1,30 @@
+import imgBin from "./images/trash-can.png";
+
+function createTaskNoteNode(task) {
+  const taskNode = document.createElement("div");
+
+  const priorityContainer = document.createElement("div");
+  priorityContainer.textContent = task.priority;
+  const dueContainer = document.createElement("div");
+  dueContainer.textContent = task.dueDate;
+
+  const header = document.createElement("div");
+  header.classList.add("header");
+  header.append(priorityContainer, dueContainer);
+
+  const taskTitle = document.createElement("p");
+  taskTitle.textContent = task.title;
+
+  const btnDelete = document.createElement("button");
+  btnDelete.classList.add("btn-delete");
+  btnDelete.setAttribute("background", imgBin);
+  btnDelete.textContent = "Remove";
+  const footer = document.createElement("div");
+  footer.appendChild(btnDelete);
+
+  taskNode.append(header, taskTitle, footer);
+  taskNode.classList.add("note-view");
+  return taskNode;
+}
+
+export { createTaskNoteNode };
