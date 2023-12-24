30226c6ead84908bd69d2fce240f4014221998f2
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-24 22:50:15 +0800

    add dummy content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("it workaaaaaaaaass!");
+import { Task, TaskManager } from "./task";
+import { createTaskNoteNode } from "./taskNode";
+import { createListNode } from "./task-lists";
+import "./style.css";
+
+const task1 = new Task("Task Title", "Task description", "Task duedate", 1);
+const task2 = new Task("bb", "cc", "not now", 5);
+
+TaskManager.addTask(task1);
+
+const taskNode = createTaskNoteNode(task1);
+document.querySelector(".tasks").appendChild(taskNode);
+document.querySelector(".task-lists").appendChild(createListNode("list 1- Favorites"));
+
+console.log(TaskManager.getTasks());
