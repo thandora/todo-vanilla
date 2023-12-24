f56e2a2d8589fef0c3b45cf99a93b8f7d409f98f
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-24 19:21:53 +0800

    add taskManager object

--- src/task.js
+++ src/task.js
@@ -1,5 +1,8 @@
+let idCounter = 0;
+
 class Task {
   constructor(title, description, dueDate, priority) {
+    this.id = ++idCounter;
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
@@ -7,4 +10,22 @@ class Task {
   }
 }
 
-export { Task };
+const TaskManager = (function () {
+  const tasks = [];
+
+  function addTask(task) {
+    tasks.push(task);
+  }
+
+  function removeTask(taskId) {
+    tasks = tasks.filter((task) => task.id !== taskId);
+  }
+
+  function getTasks() {
+    return this.tasks;
+  }
+
+  return { addTask, removeTask, getTasks };
+})();
+
+export { Task, TaskManager };
