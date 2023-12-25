2e6fd3aa91bdc578a76ff363837d213a8fafea66
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-25 21:19:48 +0800

    create ListManager class
    ListManager is responsible for holding task lists (a collection of tasks).

---
+++ src/list-manager.js
@@ -0,0 +1,14 @@
+class ListManager {
+  // Class for holding lists.
+  constructor() {
+    this.lists = [];
+  }
+
+  addList(list) {
+    this.lists.push(list);
+  }
+
+  removeList(listId) {
+    this.lists = this.lists.filter((list) => list.id !== listId);
+  }
+}
