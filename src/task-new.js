27d0402de5a729cf3d287c786c782028d1380611
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-12-31 08:33:39 +0800

    add default title on task creation

--- src/task-new.js
+++ src/task-new.js
@@ -1,19 +1,27 @@
 import { loadTaskView } from "./load-task-view";
 import { Task } from "./task";
-import { listManager } from "./list-manager";
 import { getCurrentList } from "./task-edit";
+import { loadTasks, clearNode } from "./load-tasks";
+
+const TASKS_CONTAINER_CLASS = "tasks";
 
 function attachNewTask() {
   // Add event listener to "add new task" button which allows creation
   // of new task
   const btnNewTask = document.querySelector(".tasks-container .title-container .add-list");
-  btnNewTask.addEventListener("click", newTaskEvent);
+  btnNewTask.addEventListener("click", () => {
+    newTaskEvent();
+    const titleNode = document.querySelector("textarea");
+    titleNode.select();
+  });
 }
 
 function newTaskEvent() {
-  const newTask = new Task("", "", "", "2", new Date());
+  const newTask = new Task("Untitled task", "", "", "2", new Date());
   const currentList = getCurrentList();
   currentList.addTask(newTask);
+  clearNode(`.${TASKS_CONTAINER_CLASS}`);
+  loadTasks(currentList.tasks, TASKS_CONTAINER_CLASS);
   loadTaskView(newTask);
 }
 
