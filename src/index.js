4ee0cd85d6281d45220309ba21ca278437f444a5
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-01-05 20:54:15 +0800

    add dist directory

--- src/index.js
+++ src/index.js
@@ -15,15 +15,13 @@ import { loadLocalListManager } from "./local-storage-fns";
 
 // Tasks container class
 const TASKS_CONTAINER_CLASS = "tasks";
+
 // Load listManager, from localStorage or from sample data.
 if (localStorage.getItem("listManager") !== null) {
   loadLocalListManager();
-  console.log("IF HAS RUN");
 } else {
   sampleLists.forEach((l) => listManager.addList(l));
 }
-
-console.log(listManager);
 //// Load DOM
 loadLists(listManager.lists);
 const listNodes = document.querySelectorAll(".btn-list");
